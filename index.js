1ced3efc10596867ec653630a40e89ea926f3d7f
Calvin <calvinlin6@gmail.com>
2022-12-26 19:16:08 -0500

    commit tree eb4cb9bbea3ef9f167b378d644170de2937afd84

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+
+function writeCards( names, event) {
+    let completedCards = []
+    for(let i = 0; i < names.length; i++){
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards[i]);
+        completedCards;
+    }
+}
+
+writeCards(names, "Surprise")
